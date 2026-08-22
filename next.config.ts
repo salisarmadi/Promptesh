faf4af60fd0cdbee561ab95c143e264cedc6395b
salisarmadi <155059770+salisarmadi@users.noreply.github.com>
2026-08-23 00:43:35 +0330

    Configure Supabase image storage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lloigqtejbrndznjnlwr.supabase.co",
+        pathname: "/storage/v1/object/public/Gallery/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
